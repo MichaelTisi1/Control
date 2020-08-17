adfe315832373f1271581661aeae83f1f917d62b
Jordan Hong <jordanhong89@gmail.com>
2020-08-17 15:55:35 -0400

    Completed software prototype, ready for integration with modules

--- smartUV.py
+++ smartUV.py
@@ -1,31 +1,68 @@
 """ Main class for top level control.  
 """
-# Dependency
-## Modules: ultrasonic, PIR, timer classes
+# Note:
+#   All GPIO pins will follow BCM (Broadcom) convention instead of board convention. 
 
 # Status:   in progress.
 # Last edit: Jordan Hong, 22:30 July 30, 2020 
+
+# Dependency###################################################
+## Modules: ultrasonic, PIR, timer classes
+## Assumed to have following methods from the above classes
+## Ultrasonic
+##  - getReadings(): Returns the readings of the ultrasonic sensor. (number in meters)
+
+## PIR 
+##  - getReadings(): Returns the readings of the PIR sensor. (True/False)
+
+## Timer
+##  - start(): Starts the timer.
+##  - timeElapsed(): returns the time elapsed.
+##  - stop(): Stops the timer.
+
+## Commander
+##  - Connected():  returns if connected with PyUI (True/False)
+##  - getState():   returns the state that the commander asserts (1 for lamp on, 0 for lamp off)
+##  - confirmState(state): informs the commander of the current lamp state (1/0)
+import RPi.GPIO as GPIO
+import controlLamp
+
 
 class smartUV:
     # Declare constants 
 
     # State constants
-    IDLE    = 0
-    INITIAL = 1
-    ACTIVE  = 2
+    DETECT  = 0
+    IDLE    = 1
+    INITIAL = 2
+    ACTIVE  = 3
 
-    # GPIO (subject to change)
+    # GPIO in BCM mode 
     ## Output
-    GPIO_warning    = 31
-    GPIO_lamp       = 37
+    GPIO_warning    = 6 
+    GPIO_lamp       = 26 
     ## Input
-    GPIO_PIR0       = 21
-    GPIO_PIR1       = 23
-    GPIO_PIR2       = 29
+    GPIO_PIR0       = 9 
+    GPIO_PIR1       = 11 
+    GPIO_PIR2       = 5
 
-    GPIO_DIST0      = 33
-    GPIO_DIST1      = 35
-    GPIO_DIST2      = 32
+    GPIO_DIST0      = 13
+    GPIO_DIST1      = 19
+    GPIO_DIST2      = 12
+
+
+    ## GPIO in board mode 
+    ### Output
+    #GPIO_warning    = 31
+    #GPIO_lamp       = 37
+    ### Input
+    #GPIO_PIR0       = 21
+    #GPIO_PIR1       = 23
+    #GPIO_PIR2       = 29
+
+    #GPIO_DIST0      = 33
+    #GPIO_DIST1      = 35
+    #GPIO_DIST2      = 32
 
 
     def __init__(self):
@@ -34,39 +71,123 @@ class smartUV:
         self.distanceSensor = Ultrasonic(GPIO_DIST0, GPIO_DIST1, GPIO_DIST2)
         self.motionSensor   = PIR(GPIO_PIR0, GPIO_PIR1, GPIO_PIR2)
         self.timer          = Timer()
+        self.commander      = Commander()
 
         # Declare parameters
+        self.lampON = 0
         self.state = INITIAL    ## state variable
         self.dist = 0           ## distance in cm, measured from self.distanceSensor
+        self.onTime = 0         ## Time calculated by optical parameters
         self.timeTheta = [320.25, -76.859, 444.6, -72.298]
         self.seeHuman = False   ## boolean to indicate whether human is detected
 
-        # Timing parameters
-        self.start_time = 0
-        self.current_time = 0
 
+    
     def main(self):
         """ Main function to loop through when system is not in IDLE state.
         """
-        while (self.state != IDLE):
 
-            ## Scans for moving object/human
-            self.seeHuman = self.motionSensor.getReadings()
-            if (self.seeHuman):
+        ## DETECT state
+        while (self.state == DETECT):
+            ## Checks if connected with the PyUI
+            connection = self.commander.Connected()
+            if (connection):
                 self.state = IDLE
                 break
 
-            ## Case statements
+        ## IDLE state
+        while (self.state == IDLE):
+            ## Polls commander for ON command
+            command = self.commander.getState()
+            if (command==1):
+                # on receiving ON command to start the UV lamp,
+                # Change the state to IDLE
+                self.state = INITIAL 
+                break
+
+        while (self.state in (INITIAL, ACTIVE) ):
+            
+            ### Common polling for INITIAL and ACTIVE
+            ## Polls for OFF command
+            command = self.commander.getState()
+            ## Scans for moving object/human 
+            self.seeHuman = self.motionSensor.getReadings()
+            ## If sees human or receives off command
+            if (self.seeHuman or (self.command==0)):
+                self.state = IDLE
+                break
+
+            ### Case statements
+
+            ## INITIAL state
             if (self.state==INITIAL):
                 ## Initial state, get distance readings and use it calculate UV time parameter
                 self.dist   = self.distanceSensor.getReadings()
-                self.onTime = self.distance_to_On_time(self.dist, self.timeTheta)
+                self.onTime = controlLamp.distance_to_On_time(self.dist, self.timeTheta)
+                self.state = ACTIVE
 
-            elif (self.state == ACTIVE):
-                ## Checks timer
-                ## Write to pin if needed (toggle lamp ON/OFF)
+            ## ACTIVE state
+            if (self.state == ACTIVE):
+                ## Checks if light is on, 
+                ## if off (just transitioned from INITIAL to ACTIVE), turn on
+                if (self.lampON==0):
+                    self.lamp_turnOn()          ## Turn on lamp
+                    self.warning_turnOn()       ## Turn on warning
+                    self.lampON = 1
+                    self.timer.start()          ## Starts timer
+                    self.commander.confirmState(self.lampON) ## Tells commander that light is turned on
+
+                else:
+                    time_elapsed = self.timer.timeElapsed()
+                    if (time_elapsed >= self.onTime):
+                        ## Time is up
+                        self.lamp_turnOff()     ## Turn off lamp
+                        self.warning_turnOff()  ## Turn off warning 
+                        self.timer.stop()       ## Turn off timer
+
             else:
-                ## Wrong state: outputs error (should not occur)
-                pass
+                    ## Wrong state: outputs error (should not occur)
+                    print("Wrong state")
+                    return False
         return True
 
+    def setup_GPIO(self);
+        """ Setup the GPIO pins that is interfaced directly here.
+            Ignores the grove library pins, only set up GPIO pins for warnings and UV lamp power control
+        """
+       
+        # Set board pin numbering system as BCM  
+        GPIO.setmode(GPIO.BCM)
+
+        # Set pin mode for warning and lamps as write
+        GPIO.setup(GPIO_warning, GPIO.out)
+        GPIO.setup(GPIO_lamp, GPIO.out)
+
+        return True
+
+    def lamp_turnOn(self);
+        """ Turns lamp on 
+        """
+        GPIO.output(GPIO_lamp, GPIO.HIGH)
+        self.lampON = 1
+        return True
+
+    def lamp_turnOff(self):
+        """ Turns lamp off
+        """
+        GPIO.output(GPIO_lamp, GPIO.LOW)
+        self.lampON = 0
+        return True
+        
+    def warning_turnOn(self):
+        """ Turns warning on
+        """
+        GPIO.output(GPIO_warning, GPIO.HIGH)
+        return True
+    
+    def warning_turnOff(self):
+        """ Turns warning off
+        """
+        GPIO.output(GPIO_warning, GPIO.LOW)
+        return True
+        
